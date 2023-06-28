c1db6131552a086205f68a942c2c8940895df693
Stefan Kok <kok.sjx@gmail.com>
2023-06-28 21:26:47 +0200

    smsguys sample api visual basic client code

---
+++ consume-api/KeyCloakService.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Net.Http
+Imports System.Text.Json
+Imports System.Text.Json.Serialization
+Imports System.Threading.Tasks
+Imports System.Collections.Generic
+
+Public Class TokenResponse
+    <JsonPropertyName("access_token")>
+    Public Property AccessToken As String
+    <JsonPropertyName("expires_in")>
+    Public Property ExpiresIn As Integer
+    <JsonPropertyName("refresh_expires_in")>
+    Public Property RefreshExpiresIn As Integer
+    <JsonPropertyName("refresh_token")>
+    Public Property RefreshToken As String
+    <JsonPropertyName("token_type")>
+    Public Property TokenType As String
+    <JsonPropertyName("not-before-policy")>
+    Public Property NotBeforePolicy As Integer
+    <JsonPropertyName("session_state")>
+    Public Property SessionState As String
+    <JsonPropertyName("scope")>
+    Public Property Scope As String
+End Class
+
+Public Class KeyCloakService
+    Private Const keycloakUrl As String = "https://ker.smsguys.co.za/realms/quarkus/protocol/openid-connect/token"
+    Private Const clientId As String = "backend-service"
+    Private Const clientSecret As String = "secret"
+    Private Const username As String = "alice"
+    Private Const password As String = "alice"
+
+    Public Async Function ObtainAccessTokenAsync() As Task(Of String)
+        Dim client = New HttpClient()
+
+        Dim requestBody = New Dictionary(Of String, String) From {
+            {"username", username},
+            {"password", password},
+            {"grant_type", "password"},
+            {"client_id", clientId},
+            {"client_secret", clientSecret}
+        }
+
+        Dim response = Await client.PostAsync(keycloakUrl, New FormUrlEncodedContent(requestBody))
+
+        If response.IsSuccessStatusCode Then
+            Dim jsonResponse = Await response.Content.ReadAsStringAsync()
+            Dim tokenResponse = JsonSerializer.Deserialize(Of TokenResponse)(jsonResponse)
+            Return tokenResponse.AccessToken
+        Else
+            Throw New Exception("Failed to obtain access token. Status code: " & response.StatusCode)
+        End If
+    End Function
+End Class
---
+++ consume-api/SmsService.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Net.Http
+Imports System.Text.Json
+Imports System.Text.Json.Serialization
+Imports System.Net.Http.Headers
+Imports System.Text
+
+
+Public Class SmsService
+    Private httpClient As HttpClient
+
+    Public Sub New(token As String)
+
+        httpClient = New HttpClient()
+        httpClient.BaseAddress = New Uri("https://api.smsguys.co.za/")
+        httpClient.DefaultRequestHeaders.Accept.Clear()
+        httpClient.DefaultRequestHeaders.Accept.Add(New MediaTypeWithQualityHeaderValue("application/json"))
+        'Add the access token obtained to the http headers
+        httpClient.DefaultRequestHeaders.Authorization = New AuthenticationHeaderValue("Bearer", token)
+    End Sub
+
+    Public Async Function SendSmsAsync(smsContent As String) As Task(Of Boolean)
+        Dim message = New With {
+                .requestId = Guid.NewGuid().ToString(),
+                .id = Guid.NewGuid().ToString(),
+                .messages = New List(Of Object) From {
+                    New With {
+                        .messageId = Guid.NewGuid().ToString(),
+                        .content = smsContent,
+                        .phoneNumber = "string", ' Replace with actual phone number
+                        .cutOffTime = 0
+                    }
+                }
+            }
+
+        Dim jsonContent = New StringContent(JsonSerializer.Serialize(message), Encoding.UTF8, "application/json")
+        Dim response = Await httpClient.PostAsync("api/v1/sms/create", jsonContent)
+        If response.IsSuccessStatusCode Then
+            ' Handle success
+            Console.WriteLine("SMS sent successfully. Status code: " & response.StatusCode)
+            ' Read the response content And display it.
+            Dim responseContent = Await response.Content.ReadAsStringAsync()
+            Console.WriteLine("Response JSON: " & responseContent)
+            Return True
+        Else
+            ' Handle failure
+            Console.WriteLine("Failed to send SMS. Status code: " & response.StatusCode)
+            Return False
+        End If
+    End Function
+End Class
